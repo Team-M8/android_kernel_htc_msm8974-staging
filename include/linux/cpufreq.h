20325a75f86f3f9f1a3c57b059f6fbd7b4d69885
Joshua Seidel <jsseidel1@gmail.com>
2011-01-24 09:23:52 -0500

    add savagedzen gov based on smartass
    Change-Id: I6b6705826cc2c4e993d87ac8b7f50de8ecfe1b22

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -354,6 +354,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SavagedZen)
+extern struct cpufreq_governor cpufreq_gov_SavagedZen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_SavagedZen)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
