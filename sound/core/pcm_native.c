cc0b66e03a83899813b3ac256d5447575327a3b3
Takashi Iwai <tiwai@suse.de>
2012-11-07 12:42:47 +0100

    ALSA: Add a reference counter to card instance
    commit a0830dbd4e42b38aefdf3fb61ba5019a1a99ea85 upstream.
    
    For more strict protection for wild disconnections, a refcount is
    introduced to the card instance, and let it up/down when an object is
    referred via snd_lookup_*() in the open ops.
    
    The free-after-last-close check is also changed to check this refcount
    instead of the empty list, too.
    
    Reported-by: Matthieu CASTET <matthieu.castet@parrot.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	include/sound/core.h
    	sound/core/pcm_native.c
    	sound/core/sound.c
    	sound/core/sound_oss.c

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -2095,8 +2095,7 @@ static int snd_pcm_playback_open(struct inode *inode, struct file *file)
 	pcm = snd_lookup_minor_data(iminor(inode),
 				    SNDRV_DEVICE_TYPE_PCM_PLAYBACK);
 	err = snd_pcm_open(file, pcm, SNDRV_PCM_STREAM_PLAYBACK);
-	if (pcm)
-		snd_card_unref(pcm->card);
+	snd_card_unref(pcm->card);
 	return err;
 }
 
@@ -2109,8 +2108,7 @@ static int snd_pcm_capture_open(struct inode *inode, struct file *file)
 	pcm = snd_lookup_minor_data(iminor(inode),
 				    SNDRV_DEVICE_TYPE_PCM_CAPTURE);
 	err = snd_pcm_open(file, pcm, SNDRV_PCM_STREAM_CAPTURE);
-	if (pcm)
-		snd_card_unref(pcm->card);
+	snd_card_unref(pcm->card);
 	return err;
 }
 
