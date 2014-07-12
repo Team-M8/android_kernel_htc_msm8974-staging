5654654a4ebc320f6ecdd9152dcbadb159c077e1
yank555-lu <yank555.lu@gmail.com>
2014-07-12 16:37:55 +0200

    cpufreq: add yankdemand governor
    Full stock (JB) ondemand governor with changed default tunable values aimed at lower battery consumption
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -339,6 +339,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
+extern struct cpufreq_governor cpufreq_gov_yankdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
 #endif
 
 
