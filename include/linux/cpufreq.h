6e0ef24b2250216d27ddc9267236394e5c3fc462
arter97 <qkrwngud825@gmail.com>
2014-09-01 16:30:06 +0900

    arteractive: initial bring-up from SM-G900F source drop
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    
    Change-Id: I2406d75e8fc52e154be55a6e81cba5c91b48a542

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -371,6 +371,9 @@ extern struct cpufreq_governor cpufreq_gov_yankdemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ARTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_arteractive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_arteractive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
 extern struct cpufreq_governor cpufreq_gov_abyssplug;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_abyssplug)
