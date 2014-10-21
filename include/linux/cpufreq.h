ad190c50df2b0cf895d477a819d3e68df91787ac
Chet Kener <Cl3Kener@gmail.com>
2014-10-21 10:54:13 -0400

    cpufreq: Add back all Governors
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Original authorship is, quite frankly, uncertain to me.  This is what I see in the file
    
    Top of file:
     *  Copyright (C)  2001 Russell King
     *            (C)  2003 Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>.
     *                      Jun Nakajima <jun.nakajima@intel.com>
     *            (C)  2012 Dennis Rassmann <showp1984@gmail.com>
    
    Bottom of file:
    MODULE_AUTHOR("Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>");
    MODULE_AUTHOR("Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>");
    MODULE_AUTHOR("Dennis Rassmann <showp1984@gmail.com>");
    MODULE_DESCRIPTION("'cpufreq_badass' - A badass cpufreq governor based on ondemand");
    MODULE_LICENSE("GPL");
    
    I'm guessing that Dennis Rassman did a lot of rework to get this working.
    Unfortunately, I've been unable to find a clean commit history in Dennis's repos.
    I'd love for someone to hook me up with som solid commit history on this.
    
    I'd be more than happy to make the edits!  I surely didn't write it!
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	drivers/cpufreq/cpufreq_intelliactive.c
    	drivers/cpufreq/cpufreq_intellidemand.c
    	drivers/cpufreq/cpufreq_smartmax.c
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -336,6 +336,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
