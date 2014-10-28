3e34fafa203964312a1178832d19e6b8a3d3bf83
savoca <adeddo27@gmail.com>
2014-10-28 11:35:31 +0000

    lcd_notify: Only pass display state to lcd_notifer_call_chain  - We can pass 'NULL' directly to blocking_notifier_call_chain  - Remove unused struct 'lcd_event'
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -850,7 +850,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
-		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
+		lcd_notifier_call_chain(LCD_EVENT_ON_START);
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -861,10 +861,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
-		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
+		lcd_notifier_call_chain(LCD_EVENT_ON_END);
 		break;
 	case MDSS_EVENT_BLANK:
-		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START);
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata);
 		break;
@@ -873,7 +873,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
-		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		ctrl_pdata->ctrl_state &= ~CTRL_STATE_MDP_ACTIVE;
