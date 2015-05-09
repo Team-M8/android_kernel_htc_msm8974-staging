caf79fd6c3965589d77340b96e91e49efe9f65fd
Susan McCombs <smac0628@gmail.com>
2015-05-08 21:30:25 -0400

    Remove line that was supposed to be removed in pick.

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -667,7 +667,6 @@ static void dma_cache_maint_page(struct page *page, unsigned long offset,
 			if (len + offset > PAGE_SIZE)
 				len = PAGE_SIZE - offset;
 
-			if (cache_is_vipt_nonaliasing()) {
 			vaddr = kmap_high_get(page);
 			if (vaddr) {
 				vaddr += offset;
