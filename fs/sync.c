b2a9302832f0e0564ea66aeec0f9b47efdb3b362
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-24 05:49:07 -0700

    mmc: sync to caf/LA.BF.1.1.1_rb1.26, remove htc shit
    * Normally I'd be all for keeping the git history here but, fuck
      HTC and stripping comments, We're planning to move to a more vanilla
      caf-based kernel in the semi-near future anyway. This is just a quick
      fix to a really bad problem that we're experiencing on the current
      kernel.
    
    Change-Id: I378dfc3aaf46a2543d114dfb9de9c95091568bbc

--- fs/sync.c
+++ fs/sync.c
@@ -14,7 +14,6 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-#include <trace/events/mmcio.h>
 
 #include <linux/moduleparam.h>
 
@@ -79,13 +78,11 @@ static void sync_filesystems(int wait)
 
 SYSCALL_DEFINE0(sync)
 {
-	trace_sys_sync(0);
 	wakeup_flusher_threads(0, WB_REASON_SYNC);
 	sync_filesystems(0);
 	sync_filesystems(1);
 	if (unlikely(laptop_mode))
 		laptop_sync_completion();
-	trace_sys_sync_done(0);
 	return 0;
 }
 
@@ -150,9 +147,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 			
 	if (!file->f_op || !file->f_op->fsync)
 		return -EINVAL;
-	trace_vfs_fsync(file);
 	err = file->f_op->fsync(file, start, end, datasync);
-	trace_vfs_fsync_done(file);
 	return err;
 }
 EXPORT_SYMBOL(vfs_fsync_range);
