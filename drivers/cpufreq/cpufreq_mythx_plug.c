3e26a8a94567c490f875713057d67da1c7c1d834
fizbanrapper <jeremiah.akers@outlook.com>
2015-10-22 22:22:48 -0400

    cpufreq: mythx fixup #1
    Change-Id: Icc15ce724b79443df42e24327ec072ab201dfa87

--- drivers/cpufreq/cpufreq_mythx_plug.c
+++ drivers/cpufreq/cpufreq_mythx_plug.c
@@ -160,6 +160,41 @@ static bool align_windows = true;
 #define HIGHFREQ	2649600			
 #define LOWFREQ		300000	
 
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
+{
+	u64 idle_time;
+	u64 cur_wall_time;
+	u64 busy_time;
+
+	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+
+	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+
+	idle_time = cur_wall_time - busy_time;
+	if (wall)
+		*wall = jiffies_to_usecs(cur_wall_time);
+
+	return cputime_to_usecs(idle_time);
+}
+
+u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
+{
+	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
+
+	if (idle_time == -1ULL)
+		return get_cpu_idle_time_jiffy(cpu, wall);
+	else if (!io_busy)
+		idle_time += get_cpu_iowait_time_us(cpu, wall);
+
+	return idle_time;
+}
+EXPORT_SYMBOL_GPL(get_cpu_idle_time);
+
 
 /*
  * Stay at max freq for at least max_freq_hysteresis before dropping
@@ -346,7 +381,7 @@ static bool simpl_syncfreq = false;
 		if (freq == syncfreq) {
 			syncstate = true;
 		 		if (syncstate == true) {
-				simpl_timer;
+					simpl_timer;
 				}
 
 			
