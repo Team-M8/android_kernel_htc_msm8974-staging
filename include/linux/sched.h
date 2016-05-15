7baaa478fb15ddf365b2a91354206769d5d36a80
fizbanrapper <jeremiah.akers@outlook.com>
2016-05-15 00:37:32 -0400

    header: add back a missing declaration
    Change-Id: I84326c97134f4f4e44d0dd9800de20778afab256

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1977,6 +1977,14 @@ static inline void calc_load_enter_idle(void) { }
 static inline void calc_load_exit_idle(void) { }
 #endif /* CONFIG_NO_HZ */
 
+static inline void set_wake_up_idle(bool enabled)
+{
+	if (enabled)
+		current->flags |= PF_WAKE_UP_IDLE;
+	else
+		current->flags &= ~PF_WAKE_UP_IDLE;
+}
+
 #ifndef CONFIG_CPUMASK_OFFSTACK
 static inline int set_cpus_allowed(struct task_struct *p, cpumask_t new_mask)
 {
